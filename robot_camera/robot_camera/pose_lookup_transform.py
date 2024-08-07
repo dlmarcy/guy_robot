734a177c30178d2fdb6606d94d5f96267453ae86
Duane Marcy <dlmarcy@syr.edu>
2024-08-07 16:34:18 -0400

    apriltag to robot transformation

--- robot_camera/robot_camera/pose_lookup_transform.py
+++ robot_camera/robot_camera/pose_lookup_transform.py
@@ -4,8 +4,9 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import TransformStamped
-#from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
+import tf_transformations as tf
+import numpy as np
 
 class FrameListener(Node):
 
@@ -21,37 +22,61 @@ class FrameListener(Node):
         # Call on_timer function every second
         self.timer = self.create_timer(1.0, self.on_timer)
         
-        # Store the static tag frame to map transform
-        try:
-            self.tag38_to_map = self.tf_buffer.lookup_transform(
-                'tag_frame_38',
-                'map',
-                rclpy.time.Time())
-        except TransformException as ex:
-            self.get_logger().info('Could not transform tag_frame_38 to map')
-            self.tag38_to_map = TransformStamped()
-
+        self.got_static_transform = False
+        self.tag38_to_map = TransformStamped()
+        self.measured_tag38 = TransformStamped()
+        
     def on_timer(self):
-        # Look up for the transformation between the measured tag frame and robot
-        try:
-            measured_tag38 = self.tf_buffer.lookup_transform(
-                'tag36h11:38',
-                'base_footprint',
-                rclpy.time.Time())
-        except TransformException as ex:
-            self.get_logger().info('Could not transform tag36h11:38 to base_footprint')
-            return
+        # First get the static transform, then continue with measured transform
+        # Could not get the static transform from inside the constructor, don't know why????
+        if self.got_static_transform:
+            try:
+                self.measured_tag38 = self.tf_buffer.lookup_transform(
+                    'tag36h11:38',
+                    'base_footprint',
+                    rclpy.time.Time())
+            except TransformException as ex:
+                self.get_logger().info('Could not transform tag36h11:38 to base_footprint')
+                return
+        else:
+            # Look up for the static transform
+            try:
+                self.tag38_to_map = self.tf_buffer.lookup_transform(
+                    'map',
+                    'tag_frame_38',
+                    rclpy.time.Time())
+                self.got_static_transform = True
+            except TransformException as ex:
+                self.get_logger().info('Could not transform tag_frame_38 to map')
+                return
+       
+        v = self.tag38_to_map.transform.translation
+        q = self.tag38_to_map.transform.rotation
+        mat1 = np.dot(tf.translation_matrix([v.x, v.y, v.z]), tf.quaternion_matrix([q.x, q.y, q.z, q.w]))
 
+        v = self.measured_tag38.transform.translation
+        q = self.measured_tag38.transform.rotation
+        mat2 = np.dot(tf.translation_matrix([v.x, v.y, v.z]), tf.quaternion_matrix([q.x, q.y, q.z, q.w]))
+
+        mat3 = np.dot(mat1, mat2)
+        v = tf.translation_from_matrix(mat3)
+        q = tf.quaternion_from_matrix(mat3)
+
+        #self.get_logger().info('translation %s' %len(v))
+        #self.get_logger().info('rotation %s' %len(q))
         # find the unknown map to base_footprint transform
-        robot_tag38 = measured_tag38 * self.tag38_to_map
+        #robot_tag38 = measured_tag38 * self.tag38_to_map
 
         robot_pose_msg = PoseStamped()
         robot_pose_msg.header.stamp = self.get_clock().now().to_msg()
         robot_pose_msg.header.frame_id = "map"
-        robot_pose_msg.pose.position.x = robot_tag38.transform.translation.x
-        robot_pose_msg.pose.position.y = robot_tag38.transform.translation.y
-        robot_pose_msg.pose.position.z = robot_tag38.transform.translation.z
-        robot_pose_msg.pose.orientation = robot_tag38.transform.rotation
+        robot_pose_msg.pose.position.x = v[0]
+        robot_pose_msg.pose.position.y = v[1]
+        robot_pose_msg.pose.position.z = v[2]
+        robot_pose_msg.pose.orientation.w = q[3]
+        robot_pose_msg.pose.orientation.x = q[0]
+        robot_pose_msg.pose.orientation.y = q[1]
+        robot_pose_msg.pose.orientation.z = q[2]
         self.pose_publisher.publish(robot_pose_msg)
 
 def main():
